016b01cf2b763e3cf78a678ad8a05a26050b3e5f
VanF90 <buderdegi@mail.ru>
2022-11-07 19:47:28 +0300

    last http commit

---
+++ Coding/yandex_practics/14.py
@@ -0,0 +1,43 @@
+import requests
+
+
+cities = [
+    'Омск',
+    'Калининград',
+    'Челябинск',
+    'Владивосток',
+    'Красноярск',
+    'Москва',
+    'Екатеринбург'
+]
+
+
+def make_url(city):
+    # в URL задаём город, в котором узнаем погоду
+    return f'http://wttr.in/{city}'
+
+
+def make_parameters():
+    params = {
+        'format': 2,  # погода одной строкой
+        'M': ''  # скорость ветра в "м/с"
+    }
+    return params
+
+
+def what_weather(city):
+    url = make_url(city)
+    weather_parameters = make_parameters()
+    try:
+        response = requests.get(url, params=weather_parameters)
+    except requests.ConnectionError:
+        return '<сетевая ошибка>'
+    if response.status_code == 200:
+        return response.text
+    else:
+        return '<ошибка на сервере погоды>'
+
+
+print('Погода в городах:')
+for city in cities:
+    print(city, what_weather(city))
